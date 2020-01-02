d9f3c896c746bdf9e63cc1762def9222897e9c4b
Mustafa Hassan <mustafawhassan@gmail.com>
2020-01-02 07:30:42 +0200

    portfolio v1 build

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Mustafa Hassan",
+    "role": "Full Stack Software Engineer",
+    "linkedinId": "https://www.linkedin.com/in/mustafa-hassan-280475177/",
+    "roleDescription": "I enjoy employing various parts of frontend & backend development and like learning about new technologies, writing technical articles and finding solutions to modern day problems.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mustafawhassan/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/mh2795",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"mailto:mustafawhassan@gmail.com",
+          "className":"fa fa-envelope"
+        }
+      ],
+    "aboutme":"I am a D.C. based solutions-oriented software engineer with a drive for writing clean and concise code to create a product that enhances user experience. My previous experiences in business and entrepreneurship allow me to think analytically and find unique solutions to problems you may not know exist.",
+    "address":"Washington D.C.",
+    "website":"https://mh2795.github.io",
+    "education":[
+      {
+        "UniversityName":"General Assembly",
+        "specialization":"Software Engineering Immersive",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2019"
+      },
+      {
+        "UniversityName":"George Mason University",
+        "specialization":"B.S. in Business Management",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"General Assembly",
+        "specialization":"Software Engineering Immersive",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2019",
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Postgresql"
+      },
+      {
+        "skillname":"Python"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"OTOI",
+        "description":"Personal Tracker App",
+        "imgurl": "images/portfolio/OTOI.png",
+        "projecturl": "https://mern-group-app-frontend.mustafawhassan.now.sh/"
+      },
+      {
+        "name":"NBA API",
+        "description":"Active Player Stats",
+        "imgurl":"images/portfolio/nba_api.png",
+        "projecturl": "http://github.com/mh2795"
+      },
+      {
+        "name":"CLI Contacts List",
+        "description":"Python/CLI App",  
+        "imgurl":"images/portfolio/cli_app.png",
+        "projecturl": "https://github.com/mh2795/python-command-line-application"
+      },
+      {
+        "name":"Hangman",
+        "description":"Javascript Browser Game",
+        "imgurl":"images/portfolio/hangman.png",
+        "projecturl": "https://mh2795.github.io/hangman-game/"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
